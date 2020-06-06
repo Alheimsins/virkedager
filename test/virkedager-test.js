ef146d499e1aa10e87124d56c28ae9bbb3602f35
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-06-06 09:10:23 +0200

    Adds test for crossing a year (patch)

--- test/virkedager-test.js
+++ test/virkedager-test.js
@@ -15,3 +15,8 @@ test('returns expected result with helligdager in between', t => {
   const dato = getVirkedager(new Date('2020-04-07'), 3)
   t.is(dato, '2020-04-15')
 })
+
+test('returns expected result with a new year in between', t => {
+  const dato = getVirkedager(new Date('2020-12-31'), 2)
+  t.is(dato, '2021-01-05')
+})
